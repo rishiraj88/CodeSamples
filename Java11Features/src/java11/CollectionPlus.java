65b8ac7361e8a54285a43d7de927f8cdfd1d2f18
Rishi Raj <rishiraj88+github@gmail.com>
2021-10-19 14:34:50 +0530

    Added examples for method references.

--- Java11Features/src/java11/CollectionPlus.java
+++ Java11Features/src/java11/CollectionPlus.java
@@ -15,21 +15,30 @@ public class CollectionPlus {
 		integerList.add(1);
 		integerList.add(2);
 		System.out.println("Integer list: "+integerList);
-		
-		// JDK 8
-		Integer[] integerArr = integerList.stream().toArray(size -> new Integer[size]);
-		//System.out.println("Integer array: "+Arrays.toString(integerArr));
 
 		ArrayList<String> sList = new ArrayList<>();
 		sList.add("3");
 		sList.add("4");
 		System.out.println("String list: "+sList);
 
+		// JDK 8
+		Integer[] integerArr = integerList.stream().toArray(size -> new Integer[size]);
+		//System.out.println("Integer array: "+Arrays.toString(integerArr));
+		String[] sArr = sList.stream().toArray(size -> new String[size]);
+
+
 		// JDK 11
+		//Integer[] integerArr 
 		integerArr = integerList.toArray(size -> new Integer[size]);
-		System.out.println("Integer array: "+Arrays.toString(integerArr));
-		String[] sArr = sList.toArray(size -> new String[size]);
-		System.out.println("String array: "+Arrays.toString(sArr));
+		System.out.println("\nInteger array with List.toArray(size -> new Integer[size]: "+Arrays.toString(integerArr));
+		integerArr = integerList.toArray(Integer[] :: new);
+		System.out.println("Integer array with List.toArray(Integer[] :: new): "+Arrays.toString(integerArr));
+		//String[] sArr 
+		sArr = sList.toArray(size -> new String[size]);
+		System.out.println("\nString array with List.toArray(size -> new String[size]): "+Arrays.toString(sArr));
+		sArr = sList.toArray(String[]:: new);
+		System.out.println("String array with List.toArray(String[]:: new): "+Arrays.toString(sArr));
+		
 	}
 
 }
