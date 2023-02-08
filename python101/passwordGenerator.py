a3face6e9205249d0184136e6c4742e1b817f713
Rishi Raj <rishiraj88+github@gmail.com>
2023-02-08 23:26:02 +0100

    Added authored-on date to password generator.

--- python101/passwordGenerator.py
+++ python101/passwordGenerator.py
@@ -1,6 +1,9 @@
 # to generate a random password string, given the desired length of the to-be-generated password
 # author rishiraj
+# on 20221213
+
 import random
+
 passLen = int(input("Enter the password length: "))
 s = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_+={}[]|\;:\"<>,./?" # password seed
 
