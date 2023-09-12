d71c38698f6ed81e2c3c5f83eb2aadce8a4fb723
Rishi Raj <rishiraj88+github@gmail.com>
2023-09-12 15:06:51 +0200

    test commit

--- python101/addTwoNums.py
+++ python101/addTwoNums.py
@@ -4,7 +4,7 @@ num1 = 10
 num2 = 20
 
 # style a
-print("The sum of two predefined numbers is", num1+num2)
+print("The sum of two predefined numbers is:", num1+num2)
 
 #style b
 # sum = num1+num2
@@ -16,4 +16,4 @@ print("The sum of two predefined numbers is", num1+num2)
 print("Enter two numbers to get their sum.")
 num1 = float(input("Enter a number: "))
 num2 = float(input("Enter another number:"))
-print("The sum of the user-supplied numbers is", num1+num2)
+print("The sum of the user-input numbers is:", num1+num2)
--- python101/areaOfTriangleByBaseAndHeight.py
+++ python101/areaOfTriangleByBaseAndHeight.py
@@ -1,6 +1,6 @@
-base = float(input("Enter the base measurement of right triangle: "))
-height = float(input("Enter the heignt measurement of the right triangle:"))
+base = float(input("Enter the base measurement of a right triangle: "))
+height = float(input("Enter the height measurement of the right triangle:"))
 
 area = base*height/2
 
-print("The area of the triangle is",area)
+print("The area of the triangle is:",area)
