49c0b2df34ad7313b5a77b3d17171ab87724dccd
Rishi Raj <rishiraj88+github@gmail.com>
2023-10-08 19:03:16 +0200

    Upgrade the code for the newer version of library PyPDF2.

--- python101/readPdfFileOut.py
+++ python101/readPdfFileOut.py
@@ -4,12 +4,24 @@ import PyPDF2
 speaker = pyttsx3.init()
 speaker.say("I'll read out for you.")
 
-pdfFile = open("privacyPolicy-GDPR+Wefox.pdf",mode="rb")
+#code for previous version of library PyPDF2
+'''pdfFile = open("privacyPolicy-GDPR+Wefox.pdf",mode="rb")
 reader = PyPDF2.PdfFileReader(pdfFile)
 nPages = reader.numPages
 print(nPages)
 
 pdfPage = reader.getPage(0)
 content = pdfPage.extractText()
+speaker.say(content)
+speaker.runAndWait()'''
+
+# nice and working code for currently updated version of library PyPDF2
+pdfFile = open("o2c_otc.pdf",mode="rb")
+reader = PyPDF2.PdfReader(pdfFile)
+nPages = reader.pages
+print(nPages)
+
+pdfPage = reader.pages[0]
+content = pdfPage.extract_text()
 speaker.say(content)
 speaker.runAndWait()
