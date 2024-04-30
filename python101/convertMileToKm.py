092ea315264285e750241b538cf22b8c042d629a
Rishi Raj <rishiraj88+github@gmail.com>
2024-04-30 08:54:05 +0200

    Repo overhaul: near completion.

--- python101/convertMileToKm.py
+++
@@ -1,5 +0,0 @@
-print("Distance conversion: km to miles")
-mile = float(input("Enter the distance in miles: "))
-
-km = mile * 1.60934
-print("The same distance in km is: ",km)
--- python101/powersOfTwo.py
+++
@@ -1,6 +0,0 @@
-n = int(input("To show what power of 2:"))
-
-result = list(map(lambda x: 2 ** x,range(n+1)))
-
-for i in range(n+1):
-    print("2 ^",i, "is:",result[i])
