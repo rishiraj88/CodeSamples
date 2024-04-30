c8e5481e1d655b4912d3762e24af326e2b46aa80
Rishi Raj <rishiraj88+github@gmail.com>
2024-04-30 08:21:48 +0200

    Repo overhaul: Remove Java 11 and 14 examples.

--- HelloDjangoPy/init.sh
+++
@@ -1,8 +0,0 @@
-mkdir hellodjangoproj
-cd hellodjangoproj
-python -m venv venv
-source venv/Scripts/activate
-pip install django djangorestframework
-django-admin startproject hello_django .
-django-admin startapp hello
-python manage.py runserver
